9f932ccd1ebdbdcdc5863d983f73089870bd34ef
neethuchandhavarkar <woofwoofpoodle@gmail.com>
2025-08-01 23:28:49 +1000

    Updated employer dashboard resolving all conflicts

--- app-frontend/employer-panel/src/App.js
+++ app-frontend/employer-panel/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from './pages/Login';
 import EmployerDashboard from './pages/EmployerDashboard'; 
+import CreateShift from './pages/createShift';
+
 
 function App() {
   return (
@@ -8,11 +10,13 @@ function App() {
       <nav style={{ padding: '1rem', borderBottom: '1px solid #ccc' }}>
         <Link to="/login" style={{ marginRight: '1rem' }}>Login</Link>
         <Link to="/employer-dashboard">Dashboard</Link>
+        <Link to="/create-shift"style={{ marginLeft: '1rem' }}>Create Shift</Link> 
       </nav>
 
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/employer-dashboard" element={<EmployerDashboard />} />
+         <Route path="/create-shift" element={<CreateShift />} />
       </Routes>
     </Router>
   );
