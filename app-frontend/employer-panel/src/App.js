2ccbbdc2dfc605e4230f7f2994584458d4f45344
neethuchandhavarkar <woofwoofpoodle@gmail.com>
2025-08-02 11:53:37 +1000

    Add EmployerDashboard and update App.js routing

--- app-frontend/employer-panel/src/App.js
+++ app-frontend/employer-panel/src/App.js
@@ -2,20 +2,23 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from './pages/Login';
 import EmployerDashboard from './pages/EmployerDashboard'; 
 import CreateShift from './pages/createShift';
+import GuardProfiles from './pages/GuardProfiles';
 
 function App() {
   return (
     <Router>
       <nav style={{ padding: '1rem', borderBottom: '1px solid #ccc' }}>
         <Link to="/login" style={{ marginRight: '1rem' }}>Login</Link>
-        <Link to="/employer-dashboard">Dashboard</Link>
-        <Link to="/create-shift"style={{ marginLeft: '1rem' }}>Create Shift</Link> 
+        <Link to="/employer-dashboard" style={{ marginRight: '1rem' }}>Dashboard</Link>
+        <Link to="/create-shift" style={{ marginRight: '1rem' }}>Create Shift</Link>
+        <Link to="/guard-profiles">Guard Profiles</Link>
       </nav>
 
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/employer-dashboard" element={<EmployerDashboard />} />
-         <Route path="/create-shift" element={<CreateShift />} />
+        <Route path="/create-shift" element={<CreateShift />} />
+        <Route path="/guard-profiles" element={<GuardProfiles />} />
       </Routes>
     </Router>
   );
