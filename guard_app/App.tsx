6dd086c34fc2c3eabe640e01d064f82241eefc63
Jaykumar677 <jai.jk739@gmail.com>
2025-08-10 13:19:49 +1000

    Add login/signup screens and App.tsx

--- guard_app/App.tsx
+++ guard_app/App.tsx
@@ -1,5 +1,25 @@
-import AppNavigator from './src/navigation/AppNavigator';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import LoginScreen from './src/screen/loginscreen';
+import SignupScreen from './src/screen/signupscreen';
+
+export type RootStackParamList = {
+  Login: undefined;
+  Signup: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Signup" component={SignupScreen} options={{ title: 'Sign Up' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+
 }
