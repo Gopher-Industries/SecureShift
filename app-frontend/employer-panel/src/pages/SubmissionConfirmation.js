1069d54e628efe0c917bc1b3d53fc116eac693a9
neethuchandhavarkar <woofwoofpoodle@gmail.com>
2025-08-19 19:09:40 +1000

    Adding the submission page and updated the App.js

--- app-frontend/employer-panel/src/pages/SubmissionConfirmation.js
+++ app-frontend/employer-panel/src/pages/SubmissionConfirmation.js
@@ -12,34 +12,31 @@ const SubmissionConfirmation = () => {
 
   return (
     <div className="submission-container">
+
+      {/* Main Card */}
       <div className="submission-card">
         <div className="submission-left">
-          <h3 className="brand-title">Secure Shift</h3>
           <h1 className="confirmation-title">Submission Confirmation</h1>
           <p>
-            <strong>Your submission has been received.</strong> <br />
-            Our team will review your application and the documents you
-            provided.
+            Your submission has been received. <br />
+            Our team will review your application and the documents you provided.
           </p>
           <p>
-            If your company meets the eligibility requirements, we’ll contact
-            you via email with the next steps, including login details and
-            onboarding information.
+            If your company meets the eligibility requirements, we’ll contact you via email with the next steps, including login details and onboarding information.
           </p>
           <p>
             Please allow 3–5 business days for the review process. <br />
             If you have any questions, feel free to contact us at <br />
-            <span className="support-email">support@example.com</span>.
+            [support@example.com]
           </p>
           <button className="login-btn" onClick={handleLoginRedirect}>
             Back to Log In
           </button>
         </div>
-
         <div className="submission-right">
           <img
             src={logo} 
-            alt="Secure Shift Logo"
+            alt="Secure Shift Shield Logo"
             className="logo"
           />
         </div>
