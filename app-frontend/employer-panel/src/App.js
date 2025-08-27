ab9fdb776251064f5fcd04a3100bd33d78e47121
Teddy <teddyhiny@gmail.com>
2025-08-27 20:28:19 +1000

    add header and footer, set the main content to ensures footer stays at bottom without blocking content

--- app-frontend/employer-panel/src/App.js
+++ app-frontend/employer-panel/src/App.js
@@ -5,26 +5,26 @@ import CreateShift from './pages/createShift';
 import ManageShift from './pages/ManageShift';
 import GuardProfiles from './pages/GuardProfile';
 import CompanyProfile from './pages/CompanyProfile';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
-      {<nav style={{ padding: '1rem', borderBottom: '1px solid #ccc' }}>
-        <Link to="/login" style={{ marginRight: '1rem' }}>Login</Link>
-        <Link to="/employer-dashboard">Dashboard</Link>
-        {/* <Link to="/create-shift" style={{ marginLeft: '1rem' }}>Create Shift</Link>
-        <Link to="/manage-shift" style={{ marginLeft: '1rem' }}>Manage Shift</Link>
-        <Link to="/guard-profiles" style={{ marginLeft: '1rem'}}>Guard Profile</Link> */}
-      </nav>}
-
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/employer-dashboard" element={<EmployerDashboard />} />
-        <Route path="/create-shift" element={<CreateShift />} />
-        <Route path="/manage-shift" element={<ManageShift />} />
-        <Route path="/guard-profiles" element={<GuardProfiles />} />
-        <Route path="/company-profile" element={<CompanyProfile />} />
-      </Routes>
+      <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        <Header />
+        <div style={{ flex: 1, paddingBottom: '20px' }}>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/employer-dashboard" element={<EmployerDashboard />} />
+            <Route path="/create-shift" element={<CreateShift />} />
+            <Route path="/manage-shift" element={<ManageShift />} />
+            <Route path="/guard-profiles" element={<GuardProfiles />} />
+            <Route path="/company-profile" element={<CompanyProfile />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
     </Router>
   );
 }
