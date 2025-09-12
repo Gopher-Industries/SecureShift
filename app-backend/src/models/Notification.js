719f310eb613e2018e5037204b9069f6abba5306
Muhammad Musa <44801098+musahex@users.noreply.github.com>
2025-09-13 00:17:15 +1000

    added notification status

--- app-backend/src/models/Notification.js
+++ app-backend/src/models/Notification.js
@@ -6,6 +6,7 @@ const NotificationSchema = new mongoose.Schema({
   body: { type: String, required: true },
   data: { type: Object, default: {} },
   type: { type: String, enum: ['SHIFT_NEW', 'SHIFT_UPDATE', 'MESSAGE'], required: true },
+  status: { type: String, enum: ['SENT','FAILED'], default: 'SENT' },
   sentAt: { type: Date, default: Date.now },
 }, { timestamps: true });
 
