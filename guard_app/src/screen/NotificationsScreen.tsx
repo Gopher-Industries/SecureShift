66c09344eb990a0f26ac4beb80e31e2154ae1739
chaitanyakotagiri27 <chaitanyakotagiri1@gmail.com>
2025-09-13 11:11:37 +1000

    chore(notifications): add proper stack screen props to wrapper

--- guard_app/src/screen/NotificationsScreen.tsx
+++ guard_app/src/screen/NotificationsScreen.tsx
@@ -1,7 +1,11 @@
+// guard_app/src/screen/NotificationsScreen.tsx
 import React from 'react';
-
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../navigation/AppNavigator';
 import LegacyNotifications from './notifications';
 
-export default function NotificationsScreen(props: any) {
+type Props = NativeStackScreenProps<RootStackParamList, 'Notifications'>;
+
+export default function NotificationsScreen(props: Props) {
   return <LegacyNotifications {...props} />;
 }
