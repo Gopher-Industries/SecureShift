e4fdb1a0712dd9a2f06b780c228b0accebc87173
Elijah <elijahgharvey@gmail.com>
2026-03-16 11:09:38 +1100

    Fixed issue with dropdown improperly displaying the last 2 items

--- guard_app/src/screen/DocumentsScreen.tsx
+++ guard_app/src/screen/DocumentsScreen.tsx
@@ -384,7 +384,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.1,
     shadowRadius: 8,
     elevation: 5,
-    maxHeight: 250,
+    maxHeight: 340,
   },
   dropdownItem: {
     padding: 14,
