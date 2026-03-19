dfada7a02357d8bb5a2f1fa9c4c1338e25d231ab
Krish Uppal <krishuppal@fashionsports.in>
2026-03-19 19:30:57 +1100

    Add api, layouts, and routes structure

---
+++ app-frontend/employer-panel/src/layouts/ProtectedLayout.jsx
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import ProtectedRoute from "../routes/ProtectedRoute";
+
+const ProtectedLayout = () => {
+  return (
+    <ProtectedRoute>
+      <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+        <Header />
+        <main style={{ flex: 1, paddingBottom: "20px" }}>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </ProtectedRoute>
+  );
+};
+
+export default ProtectedLayout;
